34df5101bf73235f28270f4ef8ad3d1621ce4a0a
ErnieBuyco <buycoernie@gmail.com>
2019-12-30 23:55:44 +0800

    users__auth

--- client/configs/users.js
+++ client/configs/users.js
@@ -13,6 +13,23 @@ class UserRepository extends Repository {
 
           return hashed === hashedSuppliedBuf.toString('hex');
     }
+
+    async create(attrs){
+      attrs.id = this.randomId();
+
+      const salt = crypto.randomBytes(8).toString('hex');
+      const buf = await scrypt(attrs.password, salt, 64);
+
+      const records = await this.getAll();
+      const record ={
+        ...attrs,
+        password: `${buf.toString('hex')}.${salt}`
+      };
+      records.push(record)
+      await this.writeAll(records);
+
+      return record;
+    }
 }
 
 module.exports = new UserRepository('user.json');
