31cf7979d407ad38d354ecf7338a47fe18edef0c
ErnieBuyco <buycoernie@gmail.com>
2020-01-03 01:25:12 +0800

    auth-js-validations

--- client/routes/admin/auth.js
+++ client/routes/admin/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
 const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../configs/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const siginTemplate = require('../../views/admin/auth/signin');
 
-const { requireEmail , requirePassword, requirePasswordConfirmation,
-        requireEmailExists, requireValidPasswordForUser
-} = require('./validation');
+const { requireEmail ,
+        requirePassword,
+        requirePasswordConfirmation,
+        requireEmailExists,
+        requireValidPasswordForUser
+      } = require('./validation');
 
 
