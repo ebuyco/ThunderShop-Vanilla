abba59e9f1059e99ff91f42d3ff2606876e837a0
ErnieBuyco <buycoernie@gmail.com>
2020-01-02 21:11:43 +0800

    auth.js

--- client/routes/admin/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const { handleErrors } = require('./middlewares');
-const usersRepo = require('../../configs/users');
-const signupTemplate = require('../../views/admin/auth/signup');
-const signinTemplate = require('../../views/admin/auth/signin');
-
-
-const { requireEmail, requirePassword, }
